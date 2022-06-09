a7273af813db4fcbdfb350cb17d9a51c71658b03
Muthukumaran_M <muthukumaran.muthiah@qedtek.com>
2022-06-09 13:18:29 +0530

    demo code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoaderComponent } from './loader/loader.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProductsComponent } from './products/products.component';
+import { FooterMenuComponent } from './footer-menu/footer-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoaderComponent,
+    NavMenuComponent,
+    ContactComponent,
+    ProductsComponent,
+    FooterMenuComponent
   ],
   imports: [
     BrowserModule,
